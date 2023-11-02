bdf72bb3bcda5c7612286af8e2afa471e5a90f15
herbertnck <50819472+herbertnck@users.noreply.github.com>
2023-11-02 11:43:01 +0300

    Delete lib/pages/homepage1 directory

--- lib/pages/homepage1/book.dart
+++
@@ -1,8 +0,0 @@
-class Book {
-  final String title;
-  final String author;
-  final String coverImageUrl;
-
-  Book(
-      {required this.title, required this.author, required this.coverImageUrl});
-}
