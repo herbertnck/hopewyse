a865fb17e00b3adc590c0438dc80c9e614033bb4
herbertnck <50819472+herbertnck@users.noreply.github.com>
2024-07-25 09:21:23 +0300

    Update loginPage.dart

--- lib/pages/authentication/loginPage.dart
+++ lib/pages/authentication/loginPage.dart
@@ -6,12 +6,12 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'auth_service.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
   final bool isSigningIn = false;
 
   Future<void> checkInternetAndSignIn(BuildContext context) async {
     final connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.none) { 
+    if (connectivityResult == ConnectivityResult.none) {
       Fluttertoast.showToast(
         msg: 'No internet connection',
         toastLength: Toast.LENGTH_SHORT,
@@ -41,20 +41,22 @@ class LoginPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Welcome To",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
+            // const Text("Welcome To",
+            //     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
             const SizedBox(
               height: 15,
             ),
-            const Text("HopeWyse",
+            const Text("Hope Wyse",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 50,
                   color: Color.fromARGB(250, 58, 168, 193),
                 )),
             const SizedBox(
-              height: 50,
+              height: 11,
             ),
+            Image.asset('assets/images/HopeWyseCanvas.png'),
+            const SizedBox(height: 40),
             GestureDetector(
               child: OutlinedButton.icon(
                   //borderSide: const BorderSide(color: Colors.black),
