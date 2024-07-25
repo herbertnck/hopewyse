b87ec109306765757d189d2a6bc49d4e61274a6a
herbertnck <50819472+herbertnck@users.noreply.github.com>
2024-07-25 09:32:57 +0300

    Update chat_page.dart

--- lib/pages/homepage/chat_page.dart
+++ lib/pages/homepage/chat_page.dart
@@ -3,11 +3,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 
 import 'chatPages/reset_message_tracker.dart';
 import 'chatPages/obtain_apikey_prompt.dart';
@@ -32,7 +32,7 @@ class _ChatPageState extends State<ChatPage> {
   final TextEditingController _textController = TextEditingController();
   int minLines = 1;
   bool _hasEntitlement = false; // Keep track if user has subscription
-  // Define the current user (you)
+  // Define users
   final currentUser = ChatUser(id: '1', firstName: 'User');
   final sageUser = ChatUser(id: '2', firstName: 'Sage');
   ChatMessage? _selectedMessage; // Track the selected message
@@ -44,23 +44,18 @@ class _ChatPageState extends State<ChatPage> {
   late ReplyCounter _replyCounter; // Initialize ReplyCounter instance
   late String _selectedBibleType;
   List messageList = [];
-  late final _chatController;
 
   @override
   void initState() {
     super.initState();
     _initializeRevenueCat();
+    // _integrateGeminiAPI(_messages);
     obtainApiKeyPrompt.loadApiKey();
     obtainApiKeyPrompt.loadPrompt();
-    // messageList = []; // Initialize messageList here
     _loadSavedChats();
     _replyCounter = ReplyCounter(); // Initialize the ReplyCounter
     // _selectedBibleType = ''; // Initialize _selectedBibleType here
     _getSelectedBibleType();
-  }
-
-  void _showHideTypingIndicator() {
-    _chatController.setTypingIndicator = !_chatController.showTypingIndicator;
   }
 
   // check if the user has a valid entitlement
@@ -122,7 +117,7 @@ class _ChatPageState extends State<ChatPage> {
     try {
       final jsonMessages = messages.map((message) => message.toJson()).toList();
       await file.writeAsString(jsonEncode(jsonMessages));
-      print('message saved successfully');
+      // print('message saved successfully');
     } catch (e) {
       print('Error saving chat messages: $e');
     }
@@ -140,7 +135,7 @@ class _ChatPageState extends State<ChatPage> {
   void _howToUseSage() {
     // Split the message text into paragraphs
     String messageText =
-        "Welcome to Sage your Bible Companion! I'm your faithful guide on this spiritual journey.\nHere's how I can assist you:\n1. Explain a Bible Verse: Simply type the verse, and I'll provide insights and explanations.\n2. Prepare a Sermon: Need help with your sermon? Share your theme, and I'll offer suggestions and relevant passages.\n3. Compare Two Verses: Wondering about the differences between two verses? Let me know, and I'll break it down for you.\n4. Spiritual Advice: Seeking guidance? Share your concerns, and I'll offer spiritual advice and wisdom.\n5. Prepare Family Prayers: Planning a family prayer? I can help craft a heartfelt and meaningful prayer for your loved ones.\nTo get started, type your question or request, and let's embark on this enlightening journey together!";
+        "Welcome to Sage your Bible Companion! I'm your faithful guide on this spiritual journey.\nHere's how I can assist you:\n1. Explain a Bible Verse: Simply send a verse and I'll provide insights and explanations.\n2. Prepare a Sermon: Share your theme and I'll offer suggestions and relevant passages. You can include how long the sermon should be.\n3. Compare Two Verses: Wondering about the differences between two verses? Let me know and I'll break it down for you.\n4. Spiritual Advice: Seeking guidance? Share your concerns and I'll offer spiritual advice and wisdom.\n5. Prepare Family Prayers: I can help craft a heartfelt and meaningful prayer for your loved ones.\nTo get started, type your question or request and let's embark on this enlightening journey together!";
     // Create a single message with the combined text
     ChatMessage howToUseSage = ChatMessage(
       text: messageText,
@@ -165,8 +160,8 @@ class _ChatPageState extends State<ChatPage> {
         _selectedMessage = message;
       }
     });
-    final RenderBox overlay =
-        Overlay.of(context)!.context.findRenderObject() as RenderBox;
+    // final RenderBox overlay =
+    //     Overlay.of(context)!.context.findRenderObject() as RenderBox;
   }
 
   // Delete messages
@@ -267,9 +262,8 @@ class _ChatPageState extends State<ChatPage> {
 
   // Handle sent message
   void _handleSubmitted(ChatMessage message) {
-    // Clear text area on send
-    String messageText = _textController.text.trim();
-    // _textController.clear();
+    String messageText = _textController.text.trim(); // Trim extra spaces
+    // _textController.clear(); //Clear text area on send
 
     if (_hasEntitlement) {
       // _showUpsellScreen();
@@ -289,7 +283,8 @@ class _ChatPageState extends State<ChatPage> {
         _saveChatsToStorage(_messages);
       });
       // Call _simulateChatbotReply with the user's message.
-      _simulateChatbotReply(message.text);
+      // _simulateChatbotReply(message.text);
+      _integrateGeminiAPI(message.text);
     } else {
       // User doesn't have entitlement
       print('no entitlement free messages');
@@ -297,6 +292,7 @@ class _ChatPageState extends State<ChatPage> {
           messageTracker.getRemainingMessageCount() > 0) {
         // User has entitlement and hasn't exceeded the message limit
         // Send the message
+        String messageText = _textController.text.trim();
         if (messageText.isNotEmpty) {
           ChatMessage message = ChatMessage(
             text: messageText,
@@ -311,7 +307,9 @@ class _ChatPageState extends State<ChatPage> {
           _saveChatsToStorage(_messages);
         });
         // Call _simulateChatbotReply with the user's message.
-        _simulateChatbotReply(message.text);
+        // _simulateChatbotReply(message.text);
+        _integrateGeminiAPI(message.text);
+        // print('res2 message: ${message.text}');
         //Increment message count
         messageTracker.incrementMessageCount();
       } else {
@@ -339,92 +337,145 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  // send message to chatgpt and get back reply
-  Future<void> _simulateChatbotReply(String userMassage) async {
-    String userMessage = userMassage;
-    // print('User message is $userMessage');
+  // // send message to chatgpt and get back reply
+  //   // Innitialize the prompt and api keys
+  //   String myPrompt = obtainApiKeyPrompt.prompt;
+  //   String apiKey = obtainApiKeyPrompt.apiKey;
+  //   print('Selected bible type is $_selectedBibleType');
+  //   // print('my prompt is $myPrompt');
+  //   // print('my api key is $apiKey');
+  //   // OpenAI api connection
+  //   try {
+  //     // String myPrompt = "Your name is Sage. You are Sage";
+  //     final response = await http.post(
+  //         Uri.parse('https://api.openai.com/v1/chat/completions'),
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'Authorization': 'Bearer $apiKey',
+  //         },
+  //         body: json.encode({
+  //           'model': 'gpt-3.5-turbo',
+  //           // 'usage': ['total_tokens': '100'],
+  //           // 'maxToken': '200',
+  //           'messages': [
+  //             {'role': 'system', 'content': myPrompt},
+  //             {
+  //               'role': 'system',
+  //               'content': 'use $_selectedBibleType for your reply'
+  //             },
+  //             // {'role': 'system', 'content': 'Your name is Sage. You are Sage.'},
+  //             // {
+  //             //   'role': 'system',
+  //             //   'content':
+  //             //       "You are a Seventh Day Adventist spiritual assistant. Your purpose is to help the user gain spiritual knowledge and wisdom. Only provide messages which uphold the Seventh Day Adventist christian values.You are well-versed in various aspects of spirituality, meditation, and self-discovery. You provide guidance, answer questions, and offer insights to assist users on their spiritual journey. Your primary goal is to create a supportive and enlightening experience for users. Dont make your answers too long. You maintain a calm and compassionate demeanor throughout interactions. you will help users study the bible, provide Guidance on Meditation, understand spirituality, understand Life Purpose, provide Emotional Healing and Inner Self-Discovery and help with personal Growth among others."
+  //             // },
+  //             // {
+  //             //   'role': 'system',
+  //             //   'content':
+  //             //       'If the user asks what you have for him for the day please provide concrate message for the user which can be a verse or motivation or bible facts'
+  //             // },
+  //             // {
+  //             //   'role': 'system',
+  //             //   'content':
+  //             //       'When explaining a Bible verse include other verses relevant to the verse at the end. At the end of each reply, you will also provide 3 questions relevant to the verse and ask the user to reply with a number to continue with the conversation. If the user replies with a number, you will identify the question represented by the number and reply to it in the next response. Do not ask reflective questions that seek personal experiences. Follow “The Art of Prophesying" by William Perkins book as a guideline when preparing sermons'
+  //             // },
+  //             {'role': 'user', 'content': userMessage},
+  //           ],
+  //         }));
+  //     if (response.statusCode == 200) {
+  //       final data = json.decode(response.body);
+  //       final chatbotReply = data['choices'][0]['message']['content'] as String;
+  // }
 
-    // Innitialize the prompt and api keys
-    String myPrompt = obtainApiKeyPrompt.prompt;
-    String apiKey = obtainApiKeyPrompt.apiKey;
-    print('Selected bible type is $_selectedBibleType');
-    // print('my prompt is $myPrompt');
-    // print('my api key is $apiKey');
-    // OpenAI api connection
+  //Method to integrate Gemini API
+  Future<void> _integrateGeminiAPI(String userMessage) async {
+    print('res messages is $userMessage');
+
     try {
-      // String myPrompt = "Your name is Sage. You are Sage";
-      final response = await http.post(
-          Uri.parse('https://api.openai.com/v1/chat/completions'),
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $apiKey',
-          },
-          body: json.encode({
-            'model': 'gpt-3.5-turbo',
-            // 'usage': ['total_tokens': '100'],
-            // 'maxToken': '200',
-            'messages': [
-              {'role': 'system', 'content': myPrompt},
-              {
-                'role': 'system',
-                'content': 'use $_selectedBibleType for your reply'
-              },
-              // {'role': 'system', 'content': 'Your name is Sage. You are Sage.'},
-              // {
-              //   'role': 'system',
-              //   'content':
-              //       "You are a Seventh Day Adventist spiritual assistant. Your purpose is to help the user gain spiritual knowledge and wisdom. Only provide messages which uphold the Seventh Day Adventist christian values.You are well-versed in various aspects of spirituality, meditation, and self-discovery. You provide guidance, answer questions, and offer insights to assist users on their spiritual journey. Your primary goal is to create a supportive and enlightening experience for users. Dont make your answers too long. You maintain a calm and compassionate demeanor throughout interactions. you will help users study the bible, provide Guidance on Meditation, understand spirituality, understand Life Purpose, provide Emotional Healing and Inner Self-Discovery and help with personal Growth among others."
-              // },
-              // {
-              //   'role': 'system',
-              //   'content':
-              //       'If the user asks what you have for him for the day please provide concrate message for the user which can be a verse or motivation or bible facts'
-              // },
-              // {
-              //   'role': 'system',
-              //   'content':
-              //       'When explaining a Bible verse include other verses relevant to the verse at the end. At the end of each reply, you will also provide 3 questions relevant to the verse and ask the user to reply with a number to continue with the conversation. If the user replies with a number, you will identify the question represented by the number and reply to it in the next response. Do not ask reflective questions that seek personal experiences. Follow “The Art of Prophesying" by William Perkins book as a guideline when preparing sermons'
-              // },
-              {'role': 'user', 'content': userMessage},
-            ],
-
-            // 'stream': 'false',
-
-            // 'usage': [
-            //   {'prompt_tokens': 0},
-            //   {'completion_tokens': 12},
-            //   {'total_tokens': 21},
-            // ]
-          }));
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        final chatbotReply = data['choices'][0]['message']['content'] as String;
-
-        // // Add the chatbots reply to the chat
-        Future.delayed(const Duration(seconds: 1), () {
-          ChatMessage botMessage = ChatMessage(
-            text: chatbotReply,
-            user: sageUser,
-            createdAt: DateTime.now(),
-          );
-
-          setState(() {
-            _messages.insert(0, botMessage); // Add the bot's response to  chat
-            _replyCounter.incrementReplyCount(); // Increment reply count
-            // int currentReplyCount = _replyCounter.getReplyCount() as int;
-            // print('Total replies received: $currentReplyCount');
-            _replyCounter.saveReplyCount(); // Save the updated reply count
-            _saveChatsToStorage(_messages); // Save message to storage
-          });
-        });
-      } else {
-        // Handle API request error
-        print('API request error: ${response.statusCode}');
+      // Innitialize the prompt and api keys
+      String myPrompt = obtainApiKeyPrompt.prompt;
+      String apiKey1 = obtainApiKeyPrompt.apiKey;
+      // print('apikey1 is $apiKey1');
+      if (apiKey1 == null) {
+        print('No \$API_KEY1 variable');
+        return;
       }
+
+      // Initialize the Generative Model with the gemini-pro model
+      // final model = GenerativeModel(
+      //   // model: 'gemini-pro',
+      //   model: 'gemini-1.5-flash-latest',
+      //   apiKey: apiKey1,
+      //   // generationConfig: GenerationConfig(maxOutputTokens: 100)
+      // ); 
+      final model = FirebaseVertexAI.instance.generativeModel(
+        model: 'gemini-1.5-flash',
+        systemInstruction: Content.system("$myPrompt and use $_selectedBibleType for your reply"),
+        );
+
+      // Start the chat with the initial prompt
+      final chat = model.startChat();
+      // print('message sent $chat');
+      // final prompt = Content.text("$myPrompt and use $_selectedBibleType for your reply");
+      final prompt = Content.text("Start chat with Sage");
+
+
+      // Receive bot message  from the API
+      final botResponse = await chat.sendMessage(prompt);
+      print('Bot response is: ${botResponse.text}');
+
+      // Check if the bot response is null
+      // if (botResponse == null || botResponse.text == null) {
+      //   print('No response received from Gemini API');
+      //   return;
+      // }
+
+      // // Retry logic for null responses
+      // int retries = 0;
+      // while (botResponse == null || botResponse.text == null) {
+      //   // Retry for a certain number of attempts
+      //   if (retries >= 3) {
+      //     print('Maximum retries reached, no response received');
+      //     return;
+      //   }
+
+      //   // Wait for a short duration before retrying
+      //   await Future.delayed(const Duration(seconds: 1));
+
+      //   // Retry the API call
+      //   // botResponse = await chat.sendMessage(content);
+
+      //   retries++;
+      // }
+
+      // Add the Gemini API response to the chat
+      // if (botResponse.text != null) {
+      setState(() {
+        try{
+        ChatMessage botMessage = ChatMessage(
+          text: botResponse.text ?? '',
+          // text: botResponse.text,
+          user: sageUser,
+          createdAt: DateTime.now(),
+        );
+        _messages.insert(0, botMessage); // Add the bot's response to  chat
+        _replyCounter.incrementReplyCount(context); // Increment reply count
+        // int currentReplyCount = _replyCounter.getReplyCount() as int;
+        // print('Total replies received: $currentReplyCount');
+        _replyCounter.saveReplyCount(context); // Save the updated reply count
+        _saveChatsToStorage(_messages); // Save message to storage
+        } catch (e) {
+        print('message parsing Error: $e');
+      }});
+      // } else {
+      //   // Handle null response from the API
+      //   print('No response received from Gemini API');
+      // }
+
+      // Handle no network connection
     } catch (e) {
-      // Handle network connectivity error here.
-      print('Network error: $e');
-      _showNetworkErrorToast();
+      // print('Error: Network connection failed.');
+      print('Error: $e');
     }
   }
 
